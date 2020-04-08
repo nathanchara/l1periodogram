bbdebd11d42e9ba1fa4e80fcb87ed7c211427fa8
NathanHara <31962940+NathanHara@users.noreply.github.com>
2020-04-08 16:03:00 +0200

    Update combine_timeseries.py

--- l1periodogram_codes/combine_timeseries.py
+++ l1periodogram_codes/combine_timeseries.py
@@ -2,7 +2,7 @@
 
 # Copyright 2020 Nathan Hara
 #
-# This file is part of l1p.
+#  This file is part of the l1periodogram code.
 #
 # rvmodel is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
