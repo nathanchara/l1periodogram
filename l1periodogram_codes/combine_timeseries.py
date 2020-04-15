98c2dc4c31bb5d2a324964d2c42a2e8d4366e03d
NathanHara <31962940+NathanHara@users.noreply.github.com>
2020-04-15 13:35:09 +0200

    Update combine_timeseries.py

--- l1periodogram_codes/combine_timeseries.py
+++ l1periodogram_codes/combine_timeseries.py
@@ -15,7 +15,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with l1p.  If not, see <http://www.gnu.org/licenses/>.
+# along with l1periodogram.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
 sys.path.insert(0, '/Users/nathan/Documents/Python_R/lars_phi')
