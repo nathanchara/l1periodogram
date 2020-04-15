d932dc4f51e3ac2b3e336254e6c9a56c57d8d3e5
NathanHara <31962940+NathanHara@users.noreply.github.com>
2020-04-15 13:36:38 +0200

    Update l1periodogram_v1.py

--- l1periodogram_codes/l1periodogram_v1.py
+++ l1periodogram_codes/l1periodogram_v1.py
@@ -15,7 +15,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with spell1.  If not, see <http://www.gnu.org/licenses/>.
+# along with l1periodogram.  If not, see <http://www.gnu.org/licenses/>.
 
 
 
